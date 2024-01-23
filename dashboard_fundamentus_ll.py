3faad10fe4e4b82cc265a651f482ec1bbc02974f
gassuncao27 <gassuncao27@gmail.com>
2024-01-23 14:57:02 -0300

    Estudo estrategia carteira vol

--- dashboard_fundamentus_ll.py
+++ dashboard_fundamentus_ll.py
@@ -271,23 +271,23 @@ def transform_FDC_tri(fDicionario, fConta, fQuarter, fQtdtrimestres, fTrimestres
 
 # stock = ['AESB3']
 
-# stock = [
-#     'VALE3', 'PETR4', 'ELET3', 'ABEV3', 'RENT3', 'ITSA4', 'B3SA3', 'WEGE3', 'BPAC11','EQTL3',
-#     'PRIO3', 'SUZB3', 'RADL3', 'RAIL3', 'GGBR4', 'UGPA3', 'RDOR3', 'JBSS3', 'VBBR3', 'BRFS3',
-#     'VIVT3', 'CSAN3', 'ENEV3', 'HAPV3', 'CMIG4', 'SBSP3', 'TOTS3', 'KLBN11','CPLE6','ENGI11',
-#     'EMBR3', 'HYPE3', 'LREN3', 'TIMS3', 'ASAI3', 'CCRO3', 'EGIE3', 'CSNA3', 'CMIN3', 'GOAU4', 
-#     'TAEE11','RRRP3', 'MULT3', 'CPFE3', 'MGLU3', 'FLRY3', 'YDUQ3', 'AZUL4', 'CRFB3', 'CYRE3', 
-#     'COGN3', 'BRAP4', 'BRKM5','IGTI11', 'CIEL3', "TTEN3", "ABCB4", "AERI3", "AALR3", "ALPA4",
-#     "ALUP11","AMBP3", "ANIM3", "ARZZ3", "ARML3", "BMOB3", "CEAB3", "CLSA3", "CSMG3", "CURY3", 
-#     "CVCB3", "DASA3", "DXCO3", "PNVL3", "DIRR3", "ECOR3", "ENAT3", "ESPA3", "EVEN3", "EZTC3", 
-#     "FESA4", "FRAS3", "GFSA3", "GOLL4", "GGPS3", "GRND3", "SBFG3", "GUAR3", "HBSA3", "INTB3",
-#     "MYPK3", "RANI3", "JHSF3", "KEPL3", "LAVV3", "LWSA3", "LOGG3", "LOGN3", "POMO4", "MRFG3", 
-#     "MATD3", "LEVE3", "BEEF3", "MOVI3", "MRVE3", "MLAS3", "ODPV3", "ONCO3", "ORVR3", "PCAR3", 
-#     "PGMN3", "RECV3", "PETZ3", "PLPL3", "PTBL3", "POSI3", "QUAL3", "LJQQ3", "RAPT4", "ROMI3",
-#     "SAPR11","STBP3", "SEQL3", "SEER3", "SIMH3", "SLCE3", "SMFT3", "TASA4", "TGMA3", "TEND3", 
-#     "TRIS3", "TUPY3", "UNIP6", "USIM5", "VLID3", "VULC3", "WIZC3", "ZAMP3" ]
+stock = [
+    'VALE3', 'PETR4', 'ELET3', 'ABEV3', 'RENT3', 'ITSA4', 'B3SA3', 'WEGE3', 'BPAC11','EQTL3',
+    'PRIO3', 'SUZB3', 'RADL3', 'RAIL3', 'GGBR4', 'UGPA3', 'RDOR3', 'JBSS3', 'VBBR3', 'BRFS3',
+    'VIVT3', 'CSAN3', 'ENEV3', 'HAPV3', 'CMIG4', 'SBSP3', 'TOTS3', 'KLBN11','CPLE6','ENGI11',
+    'EMBR3', 'HYPE3', 'LREN3', 'TIMS3', 'ASAI3', 'CCRO3', 'EGIE3', 'CSNA3', 'CMIN3', 'GOAU4', 
+    'TAEE11','RRRP3', 'MULT3', 'CPFE3', 'MGLU3', 'FLRY3', 'YDUQ3', 'AZUL4', 'CRFB3', 'CYRE3', 
+    'COGN3', 'BRAP4', 'BRKM5','IGTI11', 'CIEL3', "TTEN3", "ABCB4", "AERI3", "AALR3", "ALPA4",
+    "ALUP11","AMBP3", "ANIM3", "ARZZ3", "ARML3", "BMOB3", "CEAB3", "CLSA3", "CSMG3", "CURY3", 
+    "CVCB3", "DASA3", "DXCO3", "PNVL3", "DIRR3", "ECOR3", "ENAT3", "ESPA3", "EVEN3", "EZTC3", 
+    "FESA4", "FRAS3", "GFSA3", "GOLL4", "GGPS3", "GRND3", "SBFG3", "GUAR3", "HBSA3", "INTB3",
+    "MYPK3", "RANI3", "JHSF3", "KEPL3", "LAVV3", "LWSA3", "LOGG3", "LOGN3", "POMO4", "MRFG3", 
+    "MATD3", "LEVE3", "BEEF3", "MOVI3", "MRVE3", "MLAS3", "ODPV3", "ONCO3", "ORVR3", "PCAR3", 
+    "PGMN3", "RECV3", "PETZ3", "PLPL3", "PTBL3", "POSI3", "QUAL3", "LJQQ3", "RAPT4", "ROMI3",
+    "SAPR11","STBP3", "SEQL3", "SEER3", "SIMH3", "SLCE3", "SMFT3", "TASA4", "TGMA3", "TEND3", 
+    "TRIS3", "TUPY3", "UNIP6", "USIM5", "VLID3", "VULC3", "WIZC3", "ZAMP3" ]
 
-stock = ['VALE3', 'PETR4']
+# stock = ['VALE3', 'PETR4']
 
 for y in range(len(stock)):
     print(stock[y])
